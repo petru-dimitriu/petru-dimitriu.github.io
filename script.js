460e1b5b258c2b8587b15dd018f186bdd6c896d9
Petru Dimitriu <dimitriupetru@gmail.com>
2016-02-01 20:28:21 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function init()
+{
+	$("h1").prop('title', getGreetingExplained());
+	$("h1").html(getGreeting);
+}
+
+function getGreeting()
+{
+	var date = new Date();
+	var hr = date.getHours();
+	if (hr >= 18 || hr <= 3)
+		return 'Bună seara!';
+	else if (hr <= 11)
+		return 'Bună dimineața!';
+	else
+		return 'Bună ziua!';
+}
+
+function getGreetingExplained()
+{
+	var date = new Date();
+	var hr = date.getHours();
+	if (hr >= 18 || hr <= 3)
+		return "'Good evening!' in Romanian";
+	else if (hr <= 11)
+		return "'Good morning!' in Romanian";
+	else
+		return "'Good afternoon!' in Romanian";
+}
